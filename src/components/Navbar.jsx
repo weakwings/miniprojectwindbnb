a098d8571630196b597c51e46689708d2f02360f
Felipe M <fehmsilva92@gmail.com>
2023-07-15 00:03:06 -0300

    inicio stays

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
 import "./Navbar.css";
+import logo from "../img/logo.png";
 
 const Navbar = () => {
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
   return (
-    <div>logo</div>
-    <div>Add location</div>
-    <div>Add guest</div>
-    <div>Search</div>
-  )
-}
-  
+    <div className="navbar">
+      <div>
+        <img className="logo" src={logo} alt="Logo" onClick={refreshPage} />
+      </div>
+      <div className="menu">
+        <div className="location">Add location</div>
+        <div className="guest">Add guest</div>
+        <div className="search1">
+          <span className="material-symbols-outlined">search</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
