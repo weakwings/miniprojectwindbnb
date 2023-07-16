31596754cd472cf66c3e857cd445856e8423548a
Felipe M <fehmsilva92@gmail.com>
2023-07-16 02:11:01 -0300

    comentarios

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from "react";
 import "./Cards.css";
 
+// The data variable is the one that will store the data from "stays.json" and setData will help us to save this data in this variable. We need to initialize this variable as an empty array to avoid errors.
 const CardsContainer = () => {
   const [data, setData] = useState([]);
 
+  // Function to get data from "stays.json".
   const getData = async () => {
+    // This try-catch statement is used to handle errors that may be generated when importing data from "stays.json".
     try {
       const res = await fetch("stays.json");
       const resJson = await res.json();
+      // Here we store the data from "stays.json" in the data variable.
       setData(resJson);
     } catch (error) {
       console.log(error);
     }
   };
-
+  // This Hook will execute the getData function every time the page is rendered.
   useEffect(() => {
     getData();
   }, []);
-
+  // JSON elements 
   return (
     <div className="cards-container">
       {data.map((el, i) => (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import logo from "../img/logo.png";
 
+/*=====Logo Refresh Page=====*/
 const Navbar = () => {
   const refreshPage = () => {
     window.location.reload();
@@ -9,13 +10,66 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div>
+        {/*=====Logo=====*/}
         <img className="logo" src={logo} alt="Logo" onClick={refreshPage} />
       </div>
       <div className="menu">
-        <div className="location">Add location</div>
-        <div className="guest">Add guest</div>
-        <div className="search1">
-          <span className="material-symbols-outlined">search</span>
+        <div>
+          {/*=====Button trigger modal=====*/}
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            <div className="location">Add location</div>
+            <div className="guest">Add guest</div>
+            <div className="search1">
+              <span className="material-symbols-outlined">search</span>
+            </div>
+          </button>
+          {/*=====Modal=====*/}
+          <div
+            className="modal fade"
+            id="exampleModal"
+            tabIndex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">
+                    Welcome to my mini-project
+                    <br />
+                    Created by{" "}
+                    <strong>
+                      <u>Felipe Messias</u>
+                    </strong>{" "}
+                    <span className="material-symbols-outlined">
+                      add_reaction
+                    </span>
+                  </h1>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body"></div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
